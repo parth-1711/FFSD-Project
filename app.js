e98228d2cf01e7ed2cdd2f01f9f1fd2b68f40442
Parth <parthirache8@gmail.com>
2023-03-20 13:03:01 +0530

    some minor changes

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
 const express=require("express");
 const bodyParser=require("body-parser");
+const ejs=require("ejs");
+const path=require("path")
+const sqlite=require("sqlite3")
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
+app.set("view engine","ejs");
+const dbna = path.join(__dirname, "data", "ofsd.db");
+
+const db=new sqlite.Database(dbna,(err)=>{
+    if (err) {
+        console.log(err.message);        
+    }
+    console.log("Connected");
+})
+
+const createTable=`create table if not exists Users (
+    uname varchar(50) primary key,
+    email varchar(100),
+    password varchar(100),
+    mobileNo varchar(12),
+    address varchar(200)
+);`
+
+db.run(createTable,(err)=>{
+    if(err){
+        console.log(err.message);
+    }
+    console.log("table created !");
+})
+
+app.get("/Table",function (req,res) {
+    const command=`select * from Users`
+    db.all(sql, (err, rows) => {
+        if (err) {
+            console.log(err.message);
+        }
+        res.render("fdata", { model: rows });
+    });
+})
 
 app.get("/login",function (req,res) {
     res.sendFile(__dirname+"/views/login.html");
@@ -41,13 +78,21 @@ app.get("/sign-up",function (req,res) {
 
 app.post("/sign-up",function (req,res) {
     let userName=req.body.username;
+    let email=req.body.email;
     let password=req.body.password;
+    let insertCommand=`insert into Users (uname,email,password) values(?,?,?)`
+    let values=[userName,email,password];
+    db.run(insertCommand,values,(err)=>{
+        if(err) console.log(err.message);
+
+    })
     console.log(userName);
     console.log(password);
     res.redirect("/home")
 })
 
-app.get("/Product",function (req,res) {
+app.get("/Product/:parameter",function (req,res) {
+    // console.log(req.params.parameter)
     res.sendFile(__dirname+"/views/Product.html")
 })
 
@@ -57,5 +102,5 @@ app.get("/sellerBargain",function (req,res) {
 
 
 app.listen(80,function () {
-    console.log("server is up and running on port 3000");
+    console.log("server is up and running");
 })
